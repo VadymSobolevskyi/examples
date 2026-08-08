da228bf9f7c9c213501e03e00cdb7d0b320c0e99
VadymSobolevskyi <35774321+VadymSobolevskyi@users.noreply.github.com>
2026-08-08 19:36:11 +0200

    removed odd files

--- Savings.h
+++
@@ -1,9 +0,0 @@
-//Savings - define a class that includes
-//the ability to make a deposit
-class Savings
-{
-    public:
-    float deposit(float amount);
-    unsigned int accountNumber;
-    float balance;
-};
